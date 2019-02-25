aba3605089e78be6930257bb96f21542f1205c06
knife1 <knife@qq.com>
2019-02-25 16:39:06 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
   }],
   deploy : {
     serverall : {
-      user : 'root', // 用户
+      user : 'tomcat', // 用户
       host : ['101.132.46.123'], // 部署服务器ip
       ref  : 'origin/master', // 远程git上面的分支
       repo : 'git@github.com:knife1/airTMC.git', // github项目路径
-      path : '/node/pm2-deploy-demo', // 部署路径
+      path : '/node/airtmc', // 部署路径
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env serverall' // 部署需执行的命令
     },
     servermeituan : {
