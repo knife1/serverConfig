7765f8d107e0ebfaa1f02ec9fced19a79d76a0e4
煜月-技术中心 <yuyue@517na.com>
2019-07-13 20:01:52 +0800

    服务发布增加配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,14 @@ module.exports = {
       path : '/node/airtmc', // 部署路径
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env serverall' // 部署需执行的命令
     },
+	serverBlogService : {
+	  user : 'root', // 用户
+      host : ['101.132.46.123'], // 部署服务器ip
+      ref  : 'origin/master', // 远程git上面的分支
+      repo : 'git@github.com:knife1/myBlogservice.git', // github项目路径
+      path : '/node/blogService', // 部署路径
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env serverBlogService' // 部署需执行的命令
+	},
     servermeituan : {
       user : 'root',
       host : ['101.132.46.123'],
